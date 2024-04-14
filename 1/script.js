a2ca603cb796dd5d274433478df7eb89155f6fb1
ikeraguero <ikerpires407@gmail.com>
2024-04-14 14:56:44 -0300

    First commit

---
+++ 1/script.js
@@ -0,0 +1,4 @@
+/* 1. Encontrar número não repetido: Escreva um algoritmo para encontrar um único
+número de um array onde cada número repete três vezes, exceto um:
+Input: [5,3,4,3,5,5,3]
+Output: 4*/
